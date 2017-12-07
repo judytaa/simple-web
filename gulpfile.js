71f6d092ac6bdb757644f5416aba55a085436ca2
judytaa <judyt.a@icloud.com>
2017-12-07 19:44:46 +0100

    correction dist and task gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,39 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var del = require("del");
+var runSequence = require('run-sequence');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+
 
 //nazwa zadania moze byc wymyslona
 //kopiowanie plików do dist
 gulp.task("copy", function() {
+// zadanie poniżej jest ok, bo tam "chwyta" plik CSS który został utworzony za pomocą zadania "sass" i przenosi go odpowiednio
     gulp.src("src/css/main.css")
         .pipe(gulp.dest("dist/css/"));
-    gulp.src("src/index.html")
-        .pipe(gulp.dest("dist/"));
+// do obrazków też jest osobne zadanie które je minimalizuje i przenosi, ale darujemy sobie to więc niech zostanie to zadanko w kopiowaniu
     gulp.src("src/img")
         .pipe(gulp.dest("dist/img/"));
-    gulp.src("src/sass")
-        .pipe(gulp.dest("dist/sass/"));
-    gulp.src("src/js")
-        .pipe(gulp.dest("dist/js/"));
+});
+
+gulp.task("js", function() {
+    // standardowo najpierw "chwytamy" to co chcemy, w tym przypadku src/js/*.js czyli wszystkie pliki w folderze js które mają rozszerzenie .js
+    gulp.src("src/js/*.js")
+    // przepuszczamy też standardowo przez naszą wtyczkę, czyli uglify()
+        .pipe(uglify())
+        // wybieramy destination, czyli ceeeel :D
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('html', function() {
+    // wybieramy plik, który chcemy zminimalizować
+    return gulp.src('src/*.html')
+    // za pomocą pipe przepuszczamy go przez nasz plugin
+        .pipe(htmlmin({collapseWhitespace: true}))
+        // podajemy ścieżkę, gdzie chcemy żeby nasz gotowy plik się znajdował
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function () {
@@ -26,6 +45,10 @@ gulp.task('sass', function () {
 
 });
 
+gulp.task('clean', function() {
+    del('dist/');
+});
+
 gulp.task('watch', function () {
     gulp.watch('src/sass/*/*.sass',['sass']); // dodatkowa gwiazdka ma wybrac wszystkie pliki z folderu
     gulp.watch(['src/*.html', 'src/js/*.js'], browserSync.reload);
@@ -34,9 +57,15 @@ gulp.task('watch', function () {
 //tworzenie "serwera"
 var browserSync = require("browser-sync").create();
 gulp.task("server", function() {
-    browserSync.init({ //odwolujemy sie do tego "pluginu" po zmiennej ktota wczesniej utworzylam
+    browserSync.init({ //odwolujemy sie do tego "pluginu" po zmiennej ktora wczesniej utworzylam
         server:"src"
     })
+});
+
+gulp.task("build", function() {
+
+    runSequence("clean", "html", "js", "copy");
+
 });
 
 gulp.task("default",["sass", "server", "watch"]); //default oznacza, ze moge wywowalc zadanie gulp - bez zbednych taskowgulp
